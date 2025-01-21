d333891b9a32429a0829529b126d318a8bbdf066
DESKTOP-CHANG\Rockh <changjy81@gmail.com>
2025-01-21 19:42:54 +0800

    add store and list functionality, disable echo

--- src/main/java/Astraea.java
+++ src/main/java/Astraea.java
@@ -1,18 +1,17 @@
-<<<<<<< HEAD
-=======
-import java.util.Objects;
->>>>>>> origin/master
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Astraea {
     static String separator = "\t____________________________________________________________";
+    static ArrayList<String> list = new ArrayList<>(100);
     static Scanner scan = new Scanner(System.in);
 
     public static void main(String[] args) {
         intro();
         String input = scan.nextLine();
         while (!input.equals("bye")) {
-            echo(input);
+            if (input.equals("list")) list();
+            else store(input);
             input = scan.nextLine();
         }
         exit();
@@ -27,6 +26,21 @@ public class Astraea {
     private static void echo(String msg) {
         System.out.println(separator);
         System.out.println("\t"+msg);
+        System.out.println(separator);
+    }
+
+    private static void store(String msg) {
+        list.add(msg);
+        System.out.println(separator);
+        System.out.println("\tadded: "+msg);
+        System.out.println(separator);
+    }
+
+    private static void list() {
+        System.out.println(separator);
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println("\t" + (i + 1) + ". " + list.get(i));
+        }
         System.out.println(separator);
     }
 
