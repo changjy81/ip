0f4d03b6cf95afb190d6ae66fae6aa6971410282
DESKTOP-CHANG\Rockh <changjy81@gmail.com>
2025-01-21 18:01:44 +0800

    rename project to Astraea, refactor, update intro and exit dialogue

---
+++ src/main/java/Astraea.java
@@ -0,0 +1,10 @@
+public class Astraea {
+    static String separator = "____________________________________________________________";
+    public static void main(String[] args) {
+        System.out.println(separator);
+        System.out.println("Astraea here. What do you want?");
+        System.out.println(separator);
+        System.out.println("Hm. Be on your way, then.");
+        System.out.println(separator);
+    }
+}
