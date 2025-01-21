9b4eda5c341a54050132e8d4c65c5c67d247be0d
DESKTOP-CHANG\Rockh <changjy81@gmail.com>
2025-01-21 18:31:33 +0800

    add indent to line, split code into functions, add echo functionality

--- src/main/java/Astraea.java
+++ src/main/java/Astraea.java
@@ -1,10 +1,34 @@
+import java.util.Scanner;
+
 public class Astraea {
-    static String separator = "____________________________________________________________";
+    static String separator = "\t____________________________________________________________";
+    static Scanner scan = new Scanner(System.in);
+
     public static void main(String[] args) {
+        intro();
+        String input = scan.nextLine();
+        while (!input.equals("bye")) {
+            echo(input);
+            input = scan.nextLine();
+        }
+        exit();
+    }
+
+    private static void intro() {
         System.out.println(separator);
-        System.out.println("Astraea here. What do you want?");
+        System.out.println("\tAstraea here. What do you want?");
         System.out.println(separator);
-        System.out.println("Hm. Be on your way, then.");
+    }
+
+    private static void echo(String msg) {
+        System.out.println(separator);
+        System.out.println("\t"+msg);
+        System.out.println(separator);
+    }
+
+    private static void exit() {
+        System.out.println(separator);
+        System.out.println("\tHm. Be on your way, then.");
         System.out.println(separator);
     }
 }
