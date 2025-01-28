45f8c4cd3d000a2006eaf5b431d596cccde14983
DESKTOP-CHANG\Rockh <changjy81@gmail.com>
2025-01-28 23:26:39 +0800

    Add file reading

--- src/main/java/Astraea.java
+++ src/main/java/Astraea.java
@@ -1,5 +1,7 @@
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -25,13 +27,17 @@ public class Astraea {
                 System.out.println("\t I have no data recorded. New storage file created.");
                 System.out.println(separator);
             } else {
-                // TODO: read data
+                // read existing save data
+                read();
                 System.out.println("\t I've retrieved your tasks from last time.");
-                System.out.println(file.getAbsoluteFile());
+                // System.out.println(file.getAbsoluteFile());
                 System.out.println(separator);
             }
         } catch (IOException e) {
             System.out.println(e.getMessage());
+        } catch (AstraeaFileException ae) {
+            // invalid/corrupted data
+            // TODO copy bad data into new file, reset to blank
         }
 
         String input = scan.nextLine();
@@ -309,6 +315,38 @@ public class Astraea {
         PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter("data/tasks.txt", true)));
         pw.println(task.getSaveStyle());
         pw.close();
+    }
+
+    private static void read() throws IOException, AstraeaFileException {
+        BufferedReader br = new BufferedReader(new FileReader("data/tasks.txt"));
+        String line;
+        Task task;
+        while ((line = br.readLine()) != null) {
+            String[] info = line.split("(\\s\\|\\s)");
+            String type = info[0];
+            boolean isDone = info[1].equals("1");
+            String name = info[2];
+            switch (type) {
+            case "T":
+                task = new Todo(name);
+                break;
+            case "D":
+                String deadline = info[3];
+                task = new Deadline(name, deadline);
+                break;
+            case "E":
+                String start = info[3];
+                String end = info[4];
+                task = new Event(name, start, end);
+                break;
+            default:
+                throw new AstraeaFileException("badFileRead");
+            }
+            list.add(task);
+            if (isDone) {
+                task.setDone();
+            }
+        }
     }
 
     private static void exit() {
