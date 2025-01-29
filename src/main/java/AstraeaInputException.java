13c3e061166c9e9f56e7b91b60b72f7bd36d2b6e
DESKTOP-CHANG\Rockh <changjy81@gmail.com>
2025-01-30 01:17:21 +0800

    Update AstraeaInputException.java
    All error messages are being printed manually to console.
    
    Let's update it to use the automatic formatting provided by
    AstraeaUI.java.

--- src/main/java/AstraeaInputException.java
+++ src/main/java/AstraeaInputException.java
@@ -1,50 +1,51 @@
 public class AstraeaInputException extends Exception {
     private final String type;
-    private static String separator = "\t____________________________________________________________";
 
     public AstraeaInputException(String type) {
         this.type = type;
     }
 
-    public void print() {
-        System.out.println(separator);
-        switch (type) {
-            case "todo_noName":
-                System.out.println("\t You should probably give an actual name to your task, hm?");
-                System.out.println("\t Usage: todo [name]");
-                break;
-            case "deadline_noName":
-                System.out.println("\t You should probably give an actual name to your task, hm?");
-                System.out.println("\t Usage: deadline [name] /by [time]");
-                break;
-            case "deadline_noTime":
-                System.out.println("\t I can't tell when you need this done by. Did you forget the divider?");
-                System.out.println("\t Usage: deadline [name] /by [time]");
-                break;
-            case "event_noName":
-                System.out.println("\t You should probably give an actual name to your task, hm?");
-                System.out.println("\t Usage: event [name] /from [time] /to [time]");
-                break;
-            case "event_noStart":
-                System.out.println("\t I can't tell when this starts. Did you forget the divider?");
-                System.out.println("\t Usage: event [name] /from [time] /to [time]");
-                break;
-            case "event_noEnd":
-                System.out.println("\t I can't tell when this ends. Did you forget the divider?");
-                System.out.println("\t Usage: event [name] /from [time] /to [time]");
-                break;
-            case "mark":
-                System.out.println("\t Give me just one number for the index of the task.");
-                System.out.println("\t Usage: mark [index]");
-                break;
-            case "unmark":
-                System.out.println("\t Give me just one number for the index of the task.");
-                System.out.println("\t Usage: unmark [index]");
-                break;
-            default:
-                System.out.println("\t What on earth are you blabbering about?");
-                System.out.println("\t Your first word isn't anything I can process.");
-        }
-        System.out.println(separator);
+    public String[] getErrorMessage() {
+        return switch (type) {
+            case "todo_noName" -> new String[]{
+                    "You should probably give an actual name to your task, hm?",
+                    "Usage: todo [name]"
+            };
+            case "deadline_noName" -> new String[]{
+                    "You should probably give an actual name to your task, hm?",
+                    "Usage: deadline [name] /by [time]"
+            };
+            case "deadline_noTime" -> new String[]{
+                    "I can't tell when you need this done by. Did you forget the divider?",
+                    "Usage: deadline [name] /by [time]"
+            };
+            case "event_noName" -> new String[]{
+                    "You should probably give an actual name to your task, hm?",
+                    "Usage: event [name] /from [time] /to [time]"
+            };
+            case "event_noStart" -> new String[]{
+                    "I can't tell when this starts. Did you forget the divider?",
+                    "Usage: event [name] /from [time] /to [time]"
+            };
+            case "event_noEnd" -> new String[]{
+                    "I can't tell when this ends. Did you forget the divider?",
+                    "Usage: event [name] /from [time] /to [time]"
+            };
+            case "mark" -> new String[]{
+                    "Give me just one number for the index of the task.",
+                    "Usage: mark [index]"
+            };
+            case "unmark" -> new String[]{
+                    "Give me just one number for the index of the task.",
+                    "Usage: unmark [index]"
+            };
+            case "empty" -> new String[]{
+                    "I can't do anything with nothing, you know."
+            };
+            default -> new String[]{
+                    "What on earth are you blabbering about?",
+                    "Your first word isn't anything I can process."
+            };
+        };
     }
 }
