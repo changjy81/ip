97f393746d326cf3e07f8fee629cdbb129f7a281
DESKTOP-CHANG\Rockh <changjy81@gmail.com>
2025-01-30 01:27:55 +0800

    Update AstraeaFIleException.java
    While not currently used, let's update to reflect the new system for
    printing error messages.

--- src/main/java/AstraeaFileException.java
+++ src/main/java/AstraeaFileException.java
@@ -1,18 +1,16 @@
 public class AstraeaFileException extends Exception {
     private final String type;
-    private static String separator = "\t____________________________________________________________";
 
     public AstraeaFileException(String type) {
         this.type = type;
     }
 
-    public void print() {
-        System.out.println(separator);
-        switch (type) {
-        default:
-            System.out.println("\t Something went wrong with reading the saved file.");
-            System.out.println("\t Copying it into backup file and resetting to blank.");
-        }
-        System.out.println(separator);
+    public String[] getErrorMessage() {
+        return switch (type) {
+            default -> new String[]{
+                "Something went wrong with reading the saved file.",
+                "Copying it into backup file and resetting to blank."
+            };
+        };
     }
 }
